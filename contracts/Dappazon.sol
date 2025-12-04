0fe569c65831e26b81770953880d9bbd0681a011
Sean Coates <sean.coates+fooyay@gmail.com>
2025-12-04 16:49:10 -0500

    initial code and tests for listing a product

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,4 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.30;
 
-contract Dappazon {}
+contract Dappazon {
+    address public owner;
+
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+
+    mapping(uint256 => Item) public items;
+
+    event Listed(uint256 id, string name, string category, string image, uint256 cost, uint256 rating, uint256 stock);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // list products
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock
+    ) public onlyOwner {
+        // create item struct
+        Item memory item = Item(_id, _name, _category, _image, _cost, _rating, _stock);
+        // save struct to blockchain
+        items[_id] = item;
+        // emit event
+        emit Listed(_id, _name, _category, _image, _cost, _rating, _stock);
+    }
+
+    // buy products
+
+    // withdraw funds
+}
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -4,6 +4,103 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-describe("Dappazon", () => {
+// global constants for listing an item
+const ID = 1
+const NAME = "Shoes"
+const CATEGORY = "Clothing"
+const IMAGE = "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg"
+const COST = tokens(1)
+const RATING = 4
+const STOCK = 5
 
+describe("Dappazon", () => {
+  let dappazon
+  let deployer, buyer
+
+  beforeEach(async () => {
+    // Setup accounts
+    [deployer, buyer] = await ethers.getSigners()
+
+    // Deploy Dappazon Contract
+    const Dappazon = await ethers.getContractFactory("Dappazon")
+    dappazon = await Dappazon.deploy()
+  })
+
+  describe("Deployment", () => {
+    it("sets the owner", async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address)
+    })
+
+
+  })
+
+  describe("Listing", () => {
+    let transaction
+
+    beforeEach(async () => {
+      transaction = await dappazon.connect(deployer).list(
+        ID,
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
+      )
+      await transaction.wait()
+    })
+
+    it("Returns item attributes after listing", async () => {
+      const item = await dappazon.items(ID)
+      expect(item.id).to.equal(ID)
+      expect(item.name).to.equal(NAME)
+      expect(item.category).to.equal(CATEGORY)
+      expect(item.image).to.equal(IMAGE)
+      expect(item.cost).to.equal(COST)
+      expect(item.rating).to.equal(RATING)
+      expect(item.stock).to.equal(STOCK)
+    })
+
+    it("Emits Listed event after listing", () => {
+      expect(transaction).to.emit(dappazon, "Listed").withArgs(
+        ID,
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
+      )
+    })
+  })
+
+  describe("onlyOwner Modifier", () => {
+    it("Allows owner to list items", async () => {
+      await expect(
+        dappazon.connect(deployer).list(
+          ID,
+          NAME,
+          CATEGORY,
+          IMAGE,
+          COST,
+          RATING,
+          STOCK
+        )
+      ).to.not.be.reverted
+    })
+
+    it("Reverts when non-owner tries to list items", async () => {
+      await expect(
+        dappazon.connect(buyer).list(
+          ID,
+          NAME,
+          CATEGORY,
+          IMAGE,
+          COST,
+          RATING,
+          STOCK
+        )
+      ).to.be.revertedWith("Only owner can call this function")
+    })
+  })
 })
