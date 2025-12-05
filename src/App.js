9b4f6669b540139d8817ac24c5a933a2ec26c18e
Sean Coates <sean.coates+fooyay@gmail.com>
2025-12-05 11:43:10 -0500

    update Navigation and connecting accounts

--- src/App.js
+++ src/App.js
@@ -13,11 +13,29 @@ import Dappazon from './abis/Dappazon.json'
 import config from './config.json'
 
 function App() {
+  const [account, setAccount] = useState(null)
+
+
+  const loadBlockchainData = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    const account = ethers.utils.getAddress(accounts[0])
+    setAccount(account)
+    console.log("Account:", account)
+    // const provider = new ethers.providers.Web3Provider(window.ethereum)
+    // const network = await provider.getNetwork()
+    // console.log("Network:", network)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
+      <Navigation account={account} setAccount={setAccount}/>
 
       <h2>Welcome to Dappazon</h2>
+      <p>{account}</p>
 
     </div>
   );
