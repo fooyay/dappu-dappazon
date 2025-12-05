27c8cb204720b14e5d0ff3d028e2acb60c88812a
Sean Coates <sean.coates+fooyay@gmail.com>
2025-12-05 10:40:49 -0500

    deploy the contract and list some items

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -12,7 +12,31 @@ const tokens = (n) => {
 }
 
 async function main() {
+  // setup accounts
+  const [deployer] = await ethers.getSigners()
 
+  // deploy Dappazon contract
+  const Dappazon = await hre.ethers.getContractFactory("Dappazon")
+  const dappazon = await Dappazon.deploy()
+  await dappazon.deployed()
+
+  console.log(`Deployed Dappazon contract at: ${dappazon.address}\n`)
+
+  // list items
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i]
+    const transaction = await dappazon.connect(deployer).list(
+      item.id,
+      item.name,
+      item.category,
+      item.image,
+      tokens(item.price),
+      item.rating,
+      item.stock
+    )
+    await transaction.wait()
+    console.log(`Listed item ${item.id}: ${item.name}`)
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
